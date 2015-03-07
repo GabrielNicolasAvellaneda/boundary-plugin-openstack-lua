178eda7f56ef4c4218ff39413baddb7ca16f33ec
gavellaneda <avellaneda.gabriel@gmail.com>
2015-03-07 17:46:58 +0000

    Changed to a Plugin and DataSource model

--- index.lua
+++ index.lua
@@ -1,10 +1,35 @@
 local framework = require('framework/framework.lua')
 local Plugin = framework.Plugin
-
+local DataSource = framework.DataSource
+local math = require('math')
 
 local params = framework.boundary.param
 params.name = 'LUA demo plugin'
 params.version = '1.0'
 
-local plugin = Plugin:new(params)
+local RandomDataSource = DataSource:extend()
+
+function RandomDataSource:initialize(minValue, maxValue)
+	self.minValue = minValue
+	self.maxValue = maxValue
+end
+
+function RandomDataSource:fetch(context, callback)
+	
+	local value = math.random(self.minValue, self.maxValue)
+	if not callback then error('fetch: you must set a callback when calling fetch') end
+
+	callback(value)
+end
+
+local dataSource = RandomDataSource:new(params.minValue, params.maxValue)
+local plugin = Plugin:new(params, dataSource)
+-- Must override to map datasource data to metrics.
+function plugin:onParseValues(data)
+	local result = {}
+	result['BOUNDARY_LUA_SAMPLE'] = tonumber(data)
+
+	return result 
+end
+
 plugin:poll()
