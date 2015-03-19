7b236d776ed4e275c9ba2fb888b5d907d3cae350
gavellaneda <avellaneda.gabriel@gmail.com>
2015-03-19 00:41:46 -0300

    Added first working version of KeystoneClient

--- init.lua
+++ init.lua
@@ -1,13 +1,113 @@
 local framework = require('framework/framework.lua')
 local Plugin = framework.Plugin
 local DataSource = framework.DataSource
+local Emitter = require('core').Emitter
+local stringutils = framework.string
 local math = require('math')
+local json = require('json')
+local net = require('net') -- TODO: This dependency will be moved to the framework.
+local http = require('http')
+require('fun')(true)
 
 local params = framework.boundary.param
-params.name = 'LUA demo plugin'
+params.name = 'Boundary OpenStack plugin'
 params.version = '1.0'
 
+local HttpDataSource = DataSource:extend()
 local RandomDataSource = DataSource:extend()
+
+local KeystoneClient = Emitter:extend() 
+
+function KeystoneClient:initialize(host, port, tenantName, username, password)
+	self.host = host
+	self.port = port
+	self.tenantName = tenantName
+	self.username = username
+	self.password = password
+end
+
+local get = framework.table.get
+
+function KeystoneClient:getToken(callback)
+	
+	local data = {auth = { tenantName = self.tenantName, passwordCredentials ={username = self.username,  password = self.password}} }
+	local path = '/v2.0/tokens'
+
+	local postData = json.stringify(data)
+
+	local headers = {}
+	headers['Content-Type'] = 'application/json'
+	headers['Content-Length'] = #postData 	
+	headers['Accept'] = 'application/json'
+
+	local reqOptions = {
+		host = self.host,
+		port = self.port,
+		path = path,
+		method = 'POST',
+		headers = headers
+	}
+
+	local req = http.request(reqOptions, function (res) 
+	
+		res:on('data', function (data) 
+			local parsed = json.parse(data)	
+			if callback then callback(parsed) end	
+			end)
+
+		res:on('error', function (err)  p(err.message) end)
+	end)
+
+	req:on('error', function (err) self:emit('error', err.message) end)
+	req:write(postData)
+	req:done()
+end
+
+local client = KeystoneClient:new('localhost', 5000, 'admin', 'admin', '123456')
+client:on('error', p)
+client:getToken(function (data)
+
+	local hasError = get('error', data)
+			if hasError ~= nil then
+				p(get('error', data))
+			else
+				p(get('id', (get('token',(get('access', data))))))
+			end
+		end)
+
+function HttpDataSource:initialize(host, port, path, username, password)
+	self.host = host
+	self.port = port
+	self.path = path
+	self.username = username
+	self.password = password
+end
+
+function HttpDataSource:hasCredentials()
+	return stringutil.notEmpty(self.username) and stringutil.notEmpty(self.password)
+end
+
+function HttpDataSource:fetch(context, callback)
+	local req = http.request(reqOptions, function (res)
+
+		res:on('data', function (chunk)
+			if callback then
+				callback(data)
+			end
+		end)
+
+		res:on('error', function (err)
+			self:emit('error', err.message)
+		end)
+
+	end)
+
+	req:on('error', function (err)
+		self:emit('error', err.message)
+	end)
+
+	req:done()
+end
 
 function RandomDataSource:initialize(minValue, maxValue)
 	self.minValue = minValue
@@ -32,4 +132,5 @@ function plugin:onParseValues(data)
 	return result 
 end
 
-plugin:poll()
+--plugin:poll()
+
